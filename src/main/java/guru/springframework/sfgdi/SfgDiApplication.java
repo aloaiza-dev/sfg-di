af88a9e0567302c49f5f9beb334bff2b24c50e09
aloaiza <alex.loaiza@gmail.com>
2020-03-02 13:05:22 -0600

    Added @Qualifiers for DI

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -31,7 +31,5 @@ public class SfgDiApplication
         System.out.println( "-------- Constructor" );
         ConstructorInjectedController constructorInjectedController = ( ConstructorInjectedController ) ctx.getBean( "constructorInjectedController" );
         System.out.println( constructorInjectedController.getGreeting() );
-
     }
-
 }
