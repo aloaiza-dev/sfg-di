30b20b87d378842294104be336593731f53ed110
Alex Loaiza <aloaiza@snapfinance.com>
2020-05-27 11:31:45 -0600

    Reading config variables from environment.

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.example.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource( "classpath:datasource.properties" )
 public class PropertyConfig
 {
+    @Autowired
+    Environment env;
+
     @Value( "${dev.username}" )
     String user;
 
@@ -24,7 +29,7 @@ public class PropertyConfig
     public FakeDataSource fakeDataSource()
     {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser( user );
+        fakeDataSource.setUser( env.getProperty( "USERNAME" ) );
         fakeDataSource.setPassword( password );
         fakeDataSource.setUrl( url );
         return fakeDataSource;
