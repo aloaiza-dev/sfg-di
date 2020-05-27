f24201fb3795da587404a857505b43ed5ab4c6a5
Alex Loaiza <aloaiza@snapfinance.com>
2020-05-27 11:25:27 -0600

    Reading config variables from property source.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.example.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,8 +16,11 @@ public class SfgDiApplication
 
         MyController controller = ( MyController ) ctx.getBean( "myController" );
 
-        System.out.println( controller.sayHello() );
-        System.out.println( ctx.getBean( PropertyInjectedController.class ).getGreeting() );
-        System.out.println( ctx.getBean( ConstructorInjectedController.class ).getGreeting() );
+        FakeDataSource fakeDataSource = ( FakeDataSource ) ctx.getBean( FakeDataSource.class );
+        System.out.println( fakeDataSource );
+
+//        System.out.println( controller.sayHello() );
+//        System.out.println( ctx.getBean( PropertyInjectedController.class ).getGreeting() );
+//        System.out.println( ctx.getBean( ConstructorInjectedController.class ).getGreeting() );
     }
 }
